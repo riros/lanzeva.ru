fdb56554ec2d84d3827290beb0db71d8a2cbb3b5
riros server <riros@tbo23.ru>
2017-05-31 15:48:03 +0300

    save

--- lanzeva/lanzeva/settings.py
+++ lanzeva/lanzeva/settings.py
@@ -15,8 +15,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '123456'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['lanzeva.ru']
 
 # Application definition
 INSTALLED_APPS = [
