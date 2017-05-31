6f829bdf71b5ef769f6faebcd778dd08abd282a4
riros server <riros@tbo23.ru>
2017-05-31 16:18:52 +0300

    save

--- lanzeva/lanzeva/settings.py
+++ lanzeva/lanzeva/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '123456'
+SECRET_KEY = '123456sdfsghhjghj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = ['lanzeva.ru']
