09b3f272915be03d9e721c2f20a1aabac7114edd
Riros ivanvalenkov@gmail.com <i.valenkov@itsirius.ru>
2017-06-15 06:29:17 +0300

    save

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 git pull
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate
-python manage.py thumbnail clear_delete_all
-systemctl restart uwsgi-app@lanzeva
-systemctl restart nginx
-chown nginx:nginx -R ../lanzeva.ru/
+#python manage.py thumbnail clear_delete_all
+#systemctl restart uwsgi-app@lanzeva
+systemctl restart nginx uwsgi
+chown www-data:www-data -R ../lanzeva.ru/
